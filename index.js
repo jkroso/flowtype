cf525f44630b4dc94fc44bc5cc9f5cca8cad09af
Jake Rosoman <jkroso@gmail.com>
2014-01-15 18:07:53 +1300

    tabs -> spaces

--- index.js
+++ index.js
@@ -12,29 +12,29 @@ var merge = require('merge')
  */
 
 module.exports = function(el, options){
-	options = merge({
-		min: 14,
-		max: 18,
-		lineRatio: 1.45,
-		minWidth: style(el).minWidth,
-		maxWidth: style(el).maxWidth
-	}, options)
+  options = merge({
+    min: 14,
+    max: 18,
+    lineRatio: 1.45,
+    minWidth: style(el).minWidth,
+    maxWidth: style(el).maxWidth
+  }, options)
 
-	var minWidth = parseFloat(options.minWidth)
-	var maxWidth = parseFloat(options.maxWidth)
-	var widthDiff = maxWidth - minWidth
-	var fontMin = options.min
-	var fontDiff = options.max - fontMin
-	var lineRatio = options.lineRatio
+  var minWidth = parseFloat(options.minWidth)
+  var maxWidth = parseFloat(options.maxWidth)
+  var widthDiff = maxWidth - minWidth
+  var fontMin = options.min
+  var fontDiff = options.max - fontMin
+  var lineRatio = options.lineRatio
 
-	function resize(){
-		var width = parseFloat(style(el).width)
-		var percent = (width - minWidth) / widthDiff
-		var font = fontMin + (fontDiff * percent)
-		el.style.lineHeight = font * lineRatio + 'px'
-		el.style.fontSize = font + 'px'
-	}
+  function resize(){
+    var width = parseFloat(style(el).width)
+    var percent = (width - minWidth) / widthDiff
+    var font = fontMin + (fontDiff * percent)
+    el.style.lineHeight = font * lineRatio + 'px'
+    el.style.fontSize = font + 'px'
+  }
 
-	viewport.on('resize', resize)
-	resize()
+  viewport.on('resize', resize)
+  resize()
 }
